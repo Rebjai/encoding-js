f42bd76b301430895555bb920982ad198d3a4e7e
rebjai <rebjai@github.com>
2020-05-22 18:22:30 -0500

    Added Comments and formated code

--- encoding.js
+++ encoding.js
@@ -1,6 +1,5 @@
 const $ = (str) => document.querySelector(str)
 
-
 //get elements
 let unipolarPositiveNonReturnZeroCanvas = document.getElementById('positiveUnipolarNonReturnToZero');
 let ctxUnipolar = unipolarPositiveNonReturnZeroCanvas.getContext("2d");
@@ -37,7 +36,11 @@ let topPoint = topMargin + 20
 let bottomPoint = unipolarPositiveNonReturnZeroCanvas.height - 20
 let midPoint = graphHeight / 2 + topMargin
 
+function clearText() {
+    $('#bits').value = ""
+    graph()
 
+}
 
 function graph() {
     bitString = $('#bits').value != '' ? $('#bits').value : '0111000101'
@@ -50,79 +53,68 @@ function graph() {
 
 }
 
-
-
 function draw(canvas = unipolarPositiveNonReturnZeroCanvas, ctx = ctxUnipolar, encoding = unipolarNonZero, startingVal = true) {
     console.log('graphing on', canvas);
     console.log('context is', ctx);
     console.log('with encoding', encoding);
 
     //setup
-    
-    canvas.width = window.innerWidth-15
+    canvas.width = window.innerWidth - 15
     sideMargin = canvas.width * marginPercetage
     graphWidth = canvas.width - (sideMargin * 2)
     graphHeight = unipolarPositiveNonReturnZeroCanvas.height - topMargin
     console.log(canvas.width);
     console.log(graphWidth);
     bitString = $('#bits').value != '' ? $('#bits').value : '0111000101'
+    let newArr = bitString.split('')
 
     //graph squares
     ctx.fillStyle = "rgb(200, 200, 200)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "rgb(180,180,180)";
-    ctx.fillRect(sideMargin-1, 0, graphWidth+sideMargin, canvas.height);
+    ctx.fillRect(sideMargin - 1, 0, graphWidth + sideMargin, canvas.height);
 
     // labels
     ctx.fillStyle = "red";
     ctx.font = "16px sans-serif";
     ctx.textBaseline = "middle"
-    ctx.fillText(label, sideMargin/4, topPoint)
-    ctx.fillText('-' + label, sideMargin/4, bottomPoint)
-    ctx.fillText(midpointLabel, sideMargin/4, midPoint)
+    ctx.fillText(label, sideMargin / 4, topPoint)
+    ctx.fillText('-' + label, sideMargin / 4, bottomPoint)
+    ctx.fillText(midpointLabel, sideMargin / 4, midPoint)
     ctx.fillStyle = "black";
-    let newArr = bitString.split('')
     getBitLabels(newArr, ctx)
 
     // horizontal lines
     drawLine(sideMargin, topMargin, canvas.width, topMargin, ctx, 'blue')
-
     drawLine(sideMargin, topPoint, canvas.width, topPoint, ctx)
     drawLine(sideMargin, bottomPoint, canvas.width, bottomPoint, ctx)
     drawLine(sideMargin, midPoint, canvas.width, midPoint, ctx)
 
     // vertical lines
-    drawVerticalLines(newArr,ctx, canvas)
+    drawVerticalLines(newArr, ctx, canvas)
 
-
+    // pass the encoding Function
     encoding(newArr, ctx, startingVal)
-
-
-
 }
 
 
-
+//Get Array of Bits to turn it into text tags on top of the graph
 function getBitLabels(arr = bitArray, ctx = ctxUnipolar) {
     let newArr = arr
     let sliceWidth = graphWidth / newArr.length
-
-
     let x = sideMargin;
+
     for (let i = 0; i < newArr.length; i++) {
         let label = newArr[i]
-        ctx.fillText(label, x+sliceWidth/2, topMargin / 2);
-
+        ctx.fillText(label, x + sliceWidth / 2, topMargin / 2);
 
         x += sliceWidth;
     }
     return newArr.length
-
 }
 
+// draw a line in the context given, you can choose a color
 function drawLine(x1, y1, x2, y2, ctx = ctxUnipolar, color = 'gray') {
-    // console.log('drawing line on', ctx);
-
     ctx.beginPath()
     ctx.strokeStyle = color
     ctx.moveTo(x1, y1)
@@ -131,63 +123,19 @@ function drawLine(x1, y1, x2, y2, ctx = ctxUnipolar, color = 'gray') {
     ctx.closePath()
 }
 
-function drawVerticalLines(arr = bitArray, ctx = ctxUnipolar, canvas = unipolarPositiveNonReturnZeroCanvas){
-    let newArr = arr
-    let sliceWidth = graphWidth / newArr.length
-
-
-    let x = sideMargin;
-    for (let i = 0; i < newArr.length; i++) {
-        
-        drawLine(x,0,x, canvas.height, ctx)
-
-        x += sliceWidth;
-    }
-
-}
-
-
-
-
-
-function manchesterize(arr = [], ctx = unipolarCtx) {
-    console.log('encoding manchester in', ctx);
-
+// draw a line in the context given.
+function drawVerticalLines(arr = bitArray, ctx = ctxUnipolar, canvas = unipolarPositiveNonReturnZeroCanvas) {
     let newArr = arr
     let sliceWidth = graphWidth / newArr.length
     let x = sideMargin;
-    ctx.strokeStyle = 'green'
-    ctx.beginPath()
-    ctx.moveTo(sideMargin, midPoint)
+
     for (let i = 0; i < newArr.length; i++) {
-        let label = arr[i]
-
-        if (label == '0') {
-            ctx.lineTo(x, topPoint)
-            ctx.lineTo(x + sliceWidth / 2, topPoint)
-            ctx.lineTo(x + sliceWidth / 2, bottomPoint)
-            ctx.lineTo(x + sliceWidth , bottomPoint)
-            // console.log('0');
-
-        }
-
-        else if (label == '1') {
-            ctx.lineTo(x , bottomPoint)
-            ctx.lineTo(x+ sliceWidth / 2, bottomPoint)
-            ctx.lineTo(x + sliceWidth / 2, topPoint)
-            ctx.lineTo(x + sliceWidth, topPoint)
-            // console.log('1');
-        }
-
+        drawLine(x, 0, x, canvas.height, ctx)
         x += sliceWidth;
     }
-    ctx.lineTo( x, midPoint)
-    
-
-    ctx.stroke()
-    ctx.closePath()
 }
 
+/***************  E N C O D I N G     F U N C T I O N S  ***************/
 
 
 function unipolarNonZero(arr = [], ctx = unipolarCtx) {
@@ -206,40 +154,34 @@ function unipolarNonZero(arr = [], ctx = unipolarCtx) {
 
         if (label == '0' && wasZero) {
             ctx.lineTo(x, midPoint)
-            // console.log('0 -> 0');
 
         } else if (label == '0' && !wasZero) {
 
-            ctx.lineTo(x , topPoint)
-            ctx.lineTo(x , midPoint)
+            ctx.lineTo(x, topPoint)
+            ctx.lineTo(x, midPoint)
             wasZero = true
-            // console.log('1 -> 0');
         }
         else if (label == '1' && wasZero) {
-            ctx.lineTo(x , midPoint)
-            ctx.lineTo(x , topPoint)
+            ctx.lineTo(x, midPoint)
+            ctx.lineTo(x, topPoint)
             wasZero = false
-            // console.log('0 -> 1');
-            // ctx.lineTo(x+ sideMargin, midPoint)
         }
         else {
             ctx.lineTo(x, topPoint)
-            // console.log('1 -> 1');
         }
 
         x += sliceWidth;
     }
     if (wasZero) {
         ctx.lineTo(x, midPoint)
-        // console.log('0 -> 0');
     } else {
         ctx.lineTo(x, topPoint)
         ctx.lineTo(x, midPoint)
-        // console.log('1 -> 1');
     }
     ctx.stroke()
     ctx.closePath()
 }
+
 function NRZL(arr = [], ctx = ctxbipolarNRZL) {
     console.log('encoding bipolar NRZL in', ctx);
 
@@ -256,78 +198,103 @@ function NRZL(arr = [], ctx = ctxbipolarNRZL) {
 
         if (label == '0' && wasZero) {
             ctx.lineTo(x, bottomPoint)
-            // console.log('0 -> 0');
 
         } else if (label == '0' && !wasZero) {
 
-            ctx.lineTo(x , topPoint)
-            ctx.lineTo(x , bottomPoint)
+            ctx.lineTo(x, topPoint)
+            ctx.lineTo(x, bottomPoint)
             wasZero = true
-            // console.log('1 -> 0');
         }
         else if (label == '1' && wasZero) {
-            ctx.lineTo(x , bottomPoint)
-            ctx.lineTo(x , topPoint)
+            ctx.lineTo(x, bottomPoint)
+            ctx.lineTo(x, topPoint)
             wasZero = false
-            // console.log('0 -> 1');
-            // ctx.lineTo(x+ sideMargin, midPoint)
         }
         else {
             ctx.lineTo(x, topPoint)
-            // console.log('1 -> 1');
         }
 
         x += sliceWidth;
     }
     if (wasZero) {
         ctx.lineTo(x, bottomPoint)
-        // console.log('0 -> 0');
     } else {
         ctx.lineTo(x, topPoint)
         ctx.lineTo(x, bottomPoint)
-        // console.log('1 -> 1');
     }
     ctx.stroke()
     ctx.closePath()
 }
+
 function NRZI(arr = [], ctx = ctxbipolarNRZI) {
     console.log('encoding bipolar NRZI in', ctx);
 
     let newArr = arr
     let sliceWidth = graphWidth / newArr.length
     let x = sideMargin;
-    ctx.moveTo(sideMargin, midPoint)
-
-    let isPositive = false
     ctx.strokeStyle = 'purple'
+
+    ctx.moveTo(sideMargin, midPoint)
+    let isPositive = false
     ctx.beginPath()
     for (let i = 0; i < newArr.length; i++) {
         let label = newArr[i]
-        
-        if(label == '1' && isPositive){
+
+        if (label == '1' && isPositive) {
             ctx.lineTo(x, topPoint)
             ctx.lineTo(x + sliceWidth, topPoint)
             isPositive = false
         }
-        else if(label == '1' && !isPositive){
+        else if (label == '1' && !isPositive) {
             ctx.lineTo(x, bottomPoint)
             ctx.lineTo(x + sliceWidth, bottomPoint)
             isPositive = true
         }
-        else if(label == '0' && isPositive){
+        else if (label == '0' && isPositive) {
             ctx.lineTo(x, bottomPoint)
             ctx.lineTo(x + sliceWidth, bottomPoint)
         }
         else {
-            
+
             ctx.lineTo(x, topPoint)
             ctx.lineTo(x + sliceWidth, topPoint)
-            
+
         }
         x += sliceWidth;
     }
     ctx.lineTo(x, midPoint)
-   
+    ctx.stroke()
+    ctx.closePath()
+}
+
+function manchesterize(arr = [], ctx = unipolarCtx) {
+    console.log('encoding manchester in', ctx);
+
+    let newArr = arr
+    let sliceWidth = graphWidth / newArr.length
+    let x = sideMargin;
+    ctx.strokeStyle = 'green'
+    ctx.beginPath()
+    ctx.moveTo(sideMargin, midPoint)
+    for (let i = 0; i < newArr.length; i++) {
+        let label = arr[i]
+
+        if (label == '0') {
+            ctx.lineTo(x, topPoint)
+            ctx.lineTo(x + sliceWidth / 2, topPoint)
+            ctx.lineTo(x + sliceWidth / 2, bottomPoint)
+            ctx.lineTo(x + sliceWidth, bottomPoint)
+        }
+        else if (label == '1') {
+            ctx.lineTo(x, bottomPoint)
+            ctx.lineTo(x + sliceWidth / 2, bottomPoint)
+            ctx.lineTo(x + sliceWidth / 2, topPoint)
+            ctx.lineTo(x + sliceWidth, topPoint)
+        }
+
+        x += sliceWidth;
+    }
+    ctx.lineTo(x, midPoint)
     ctx.stroke()
     ctx.closePath()
 }
@@ -343,57 +310,42 @@ function diffManchester(arr = [], ctx = unipolarCtx, startingVal = true) {
     ctx.strokeStyle = 'darkorange'
     ctx.beginPath()
     ctx.moveTo(x, midPoint)
-    // ctx.lineTo()
     for (let i = 0; i < newArr.length; i++) {
         let label = newArr[i]
 
         if (label == '0' && !isNegative) {
             ctx.lineTo(x, bottomPoint)
-            ctx.lineTo(x + sliceWidth/2 , bottomPoint)
-            ctx.lineTo(x+ sliceWidth/2, topPoint)
+            ctx.lineTo(x + sliceWidth / 2, bottomPoint)
+            ctx.lineTo(x + sliceWidth / 2, topPoint)
             ctx.lineTo(x + sliceWidth, topPoint)
-            // isNegative = false
-            // console.log('same positive');
 
         } else if (label == '0' && isNegative) {
 
             ctx.lineTo(x, topPoint)
-            ctx.lineTo(x+ sliceWidth / 2, topPoint)
-            ctx.lineTo(x+ sliceWidth / 2, bottomPoint)
-            ctx.lineTo(x+ sliceWidth , bottomPoint)
-            // isNegative = true
-            // console.log('same negative');
+            ctx.lineTo(x + sliceWidth / 2, topPoint)
+            ctx.lineTo(x + sliceWidth / 2, bottomPoint)
+            ctx.lineTo(x + sliceWidth, bottomPoint)
         }
         else if (label == '1' && !isNegative) {
 
-            // ctx.lineTo(x, topPoint)
             ctx.lineTo(x, bottomPoint)
             ctx.lineTo(x + sliceWidth, bottomPoint)
             isNegative = true
-            // console.log('inverse to positive');
-            // ctx.lineTo(x+ sideMargin, midPoint)
         }
         else {
             ctx.lineTo(x, topPoint)
             ctx.lineTo(x + sliceWidth, topPoint)
 
             isNegative = false
-            // console.log('inverse');
         }
-
         x += sliceWidth;
     }
-    
-        ctx.lineTo(x, midPoint)
-
+    ctx.lineTo(x, midPoint)
     ctx.stroke()
     ctx.closePath()
 }
-function clearText() {
-    $('#bits').value=""
-    graph()
-    
-}
 
-$('#bits').addEventListener('keyup',e => graph())
+//  event listeners
+
+$('#bits').addEventListener('keyup', e => graph())
 graph()
